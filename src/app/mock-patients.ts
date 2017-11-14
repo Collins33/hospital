93f6ade559e8c5cfd374f8c9c515a71ed2cf01d7
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-11-14 21:43:40 +0300

    I have been able to display the list of patients using *ngFor

---
+++ src/app/mock-patients.ts
@@ -0,0 +1,16 @@
+import {Patient} from './patient';
+//import the Patient class so that I can use it
+
+//export it so that you can use it somewhere else
+export const PATIENTS: Patient[]=[{ id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Patient } from '../patient';
+import {PATIENTS} from '../mock-patients'
+
+
+
 
 @Component({
   selector: 'app-patient',
@@ -8,6 +12,9 @@ import { Patient } from '../patient';
   encapsulation: ViewEncapsulation.None
 })
 export class PatientComponent implements OnInit {
+  //add patient property for binding
+  patients=PATIENTS;
+
   patient:Patient={
     id:1,
     name:"Collins"
