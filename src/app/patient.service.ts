214ed6f42cf964eb003008b08ddb9190f31ed2cc
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-11-19 14:49:24 +0300

    I have added the method to handle errors on the values the app retrieves from the server

--- src/app/patient.service.ts
+++ src/app/patient.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 
 @Injectable()
@@ -14,13 +15,37 @@ export class PatientService {
     //get patients using http
   getPatients(): Observable<Patient[]>{
     this.messageService.add('PatientService:fetched patients');
-    return this.http.get<Patient[]>(this.patientUrl);
+    return this.http.get<Patient[]>(this.patientUrl)
+    .pipe(
+      catchError(this.handleError('getPatients', []))
+    );
   }
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+private handleError<T> (operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
 
   getPatient(id: number): Observable<Patient[]>{
     this.messageService.add('PatientService:fetched patients id=${id}');
-    //make app get patients by using http
+   
     return this.http.get<Patient[]>(this.patientUrl);
+    
+
   }
   
   constructor(private messageService: MessageService,
